fbc600f265496a7ad1c16b5d98c1d4829665fb96
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-01-16 14:28:19 -0500

    initial commit, created folder structure for project and db, added department,role,employee table to db, setup app middleware and established connection

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    // host has to be 127.0.0.1 instead of localhost
+    host: "127.0.0.1",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password is commented out because I do not use a password
+    // password: "",
+    database: "employee-tracker",
+  },
+  console.log("Connected to the employee-tracker database.")
+);
+
+module.exports = db;
